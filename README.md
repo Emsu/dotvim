7cdb9e82db7e73c2ba2cb099e5249fb3ef23ecf3
Michael Su <mdasu1@gmail.com>
2013-02-25 15:29:21 -0800

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Emsu's .vim
+## Setup .vim by running the  deploy.sh script
--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+ln -s ~/.vim/.vimrc ~/.vimrc
